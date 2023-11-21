70c5a1517dcf9b421196d56745aadce201441d1f
D. Azevedo <rplazevedo@gmail.com>
2023-11-21 14:03:49 +0000

    Camera follow and parallax

--- Assets/Scripts/BackgroundParallax.cs
+++ Assets/Scripts/BackgroundParallax.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class BackgroundParallax : MonoBehaviour
 {
+    [SerializeField]
+    private float parallaxRate = 3.0f;
+    private Camera cam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(cam.transform.position.x / parallaxRate, 0, 0);
     }
 }
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Player player;
+
+    [SerializeField]
+    private float offset = 2.0f;
+
+    public static CameraController instance;
+
+    private void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if(player == null) { return; }
+
+        if(player.launching && player.transform.position.x >= transform.position.x - offset)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(player.transform.position.x + offset, 1, -10), Time.deltaTime * 10 );
+        }
     }
 }
